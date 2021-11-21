bafee0655a861e339615f12d1b4e25062f7fb5ed
Zapadlo <zapadlo_13@mail.ru>
2021-11-21 23:14:55 +0300

    chenge  urls and views

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -23,7 +23,7 @@ from mainapp.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('mainapp.urls'), namespase='mainapp'),
+    path('products/', include('mainapp.urls', namespace='mainapp')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,10 @@
-from django.db.migrations.state import _get_app_label_and_model_name
 from django.urls import path
-from mainapp.views import index,products
+from mainapp.views import products
 
-app_name  = 'products'
 
+app_name = 'products'
 urlpatterns = [
 
     path('', products, name='products'),
+
 ]
