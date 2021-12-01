7350cd39bc0f8bf8b5597bd7b3a7295ad3312c50
Zapadlo <zapadlo_13@mail.ru>
2021-12-01 23:34:22 +0300

    push lesson_5

---
+++ basketsapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from authapp.models import User
+from mainapp.models import Product
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+    create_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+    update_datetime = models.DateTimeField(verbose_name='время', auto_now=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def summa(self):
+        return self.product.price * self.quantity
+
+    def total_summa(self):
+        baskets = Basket.objects.filter(user = self.user)
+        return sum( basket.summa() for basket in baskets)
+
+    def total_quantity(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.quantity for basket in baskets)
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'authapp',
+    'basketsapp',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -125,3 +126,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.User'
+
+LOGIN_URL = '/users/login/'
