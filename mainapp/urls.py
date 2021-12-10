43470ab276ec773c662210e46e179df66aef8950
Zapadlo <zapadlo_13@mail.ru>
2021-12-11 00:14:59 +0300

    add paginator and selection by category

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,6 +6,8 @@ app_name = 'products'
 urlpatterns = [
 
     path('', products, name='products'),
+    path('category/<int:id_category>/', products, name='category'),
+    path('page/<int:page>/', products, name='page'),
     path('detail/<int:id>/', detail, name='detail'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -11,20 +12,33 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
+    if id_category:
+        products = Product.objects.filter(category_id=id_category)
+    else:
+        products = Product.objects.all()
+
+    paginator = Paginator(products, 3)
+
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
     context = {
         'title': 'GeekShop - Каталог',
-        'products': Product.objects.all(),
+        'products': products_paginator,
         'categories': ProductCategory.objects.all(),
     }
+
     return render(request, 'mainapp/products.html', context)
 
 
-def detail(request,id):
+def detail(request, id):
     context = {
         'title': 'GeekShop - Продукт',
-        'products': Product.objects.filter(id=id) ,
+        'product': Product.objects.get(id=id),
     }
-    return render(request, 'mainapp/products.html', context)
-
-
+    return render(request, 'mainapp/detail.html', context)
